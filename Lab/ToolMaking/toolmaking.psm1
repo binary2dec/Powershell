1845fa2dacde9781721a9e4c0b8f80685c77ab6d
binary2dec <jjzooly@gmail.com>
2017-08-07 13:15:06 -0500

    toolmaking module - v1.0.0.

--- Lab/ToolMaking/toolmaking.psm1
+++ Lab/ToolMaking/toolmaking.psm1
@@ -1,55 +1,63 @@
 Function Set-TServiceLogon {
-
-Param(
+    [CmdletBinding()]
+    Param(
+   [Parameter(ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$True,Mandatory=$True)]
     [string[]]$ComputerName,
     [string]$NewPassword,
     [string]$NewUserLogon,
-    [string]$StartName,
     [string]$Errorlogpath,
     [string]$Protocol = "wsman",
     [string]$ServiceName,
     [switch]$ProtocolFallback
     )
 
+Begin { #Nothing
 
-foreach ($computer in $ComputerName) {
-$computer | Add-Content -Path 'C:\Data\log.txt' -Verbose
+}#end Begin
+
+Process{
+    foreach ($computer in $ComputerName) {
 #Arguments
 
-if ($PSBoundParameters.ContainsKey('NewUserLogon')){
-    $args = @{'StartName' = $NewUserLogon
-              'StartPassword' = $NewPassword}
-    } #End if
-else {
-    $args = @{'StartPassword' = $NewPassword}
-} #End else
+        if ($PSBoundParameters.ContainsKey('NewUserLogon')){
+        $args = @{'StartName' = $NewUserLogon
+                  'StartPassword' = $NewPassword}
+        } #End if
+            else {
+                    $args = @{'StartPassword' = $NewPassword}
+                } #End else
 
 #Determine ProtocolOption
 
-if ($Protocol -eq "Dcom") {
+        if ($Protocol -eq "Dcom") {
 
-    $option = New-CimSessionOption -Protocol Dcom -Verbose
-} 
-else {
-    $Option = New-CimSessionOption -Protocol Wsman -Verbose
-}#end else
+        $option = New-CimSessionOption -Protocol Dcom -Verbose
+        } #end if
+            else {
+            $option = New-CimSessionOption -Protocol Wsman -Verbose
+                }#end else
 
 #Create Sessison
 
-$session = New-CimSession -ComputerName $computer -SessionOption $option -Verbose
+    $session = New-CimSession -ComputerName $computer -SessionOption $option -Verbose
 
 #Action = Call Change Method
 
-$SetService = Invoke-CimMethod -CimSession $session -MethodName Change -Query "SELECT * FROM Win32_Service WHERE Name = '$ServiceName' " -Arguments $args -ErrorAction SilentlyContinue -Verbose
+    $SetService = Invoke-CimMethod -CimSession $session -MethodName Change -Query "SELECT * FROM Win32_Service WHERE Name = '$ServiceName' " -Arguments $args -ErrorAction SilentlyContinue -Verbose
 
 #Logging
-$log = @{'ComputerName' = $computer
+    $log = @{'ComputerName' = $computer
         'ReturnValue' = $($SetService.ReturnValue) }
 
-$log | ConvertTo-Json | Add-Content -Path "C:\data\log.json"
+    $log | ConvertTo-Json | Add-Content -Path "C:\data\log.json"
 
-#DataOutput
-} #end foreach
-} #end 
+#Remove Session
+    $session | Remove-CimSession
+
+    } #end foreach
+        } #end 
+            } #end Process
+
 
 #Set-TServiceLogon -ComputerName "ms01" -ServiceName "spooler" -NewUserLogon "Local Service" 
+
